9dd19c95d2035a243f15158660041da49237b490
ella-yschoi <happymondaysu@gmail.com>
2024-07-13 04:00:17 +0900

    style: 큐레이션 카드, 큐레이터 카드 스타일 설정 변경

--- client/src/components/cards/MainCurationCard.tsx
+++ client/src/components/cards/MainCurationCard.tsx
@@ -1,10 +1,9 @@
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
-import tw from 'twin.macro';
 import styled from 'styled-components';
-
 import { AiFillHeart } from 'react-icons/ai';
+import { colors, fonts } from '../../styles/theme';
 
 import { images } from '../../utils/importImgUrl';
 import { CurationProps } from '../../types/card';
@@ -50,14 +49,15 @@ const CurationCard = ({
           <ItemLeft>
             <LikeDiv>
               <AiFillHeart />
-              좋아요 {curationLikeCount}개
+              <LikeComment>좋아요</LikeComment>
+              {curationLikeCount}개
             </LikeDiv>
           </ItemLeft>
           <ItemRight>
             <ImageDiv>
               <ProfileImg
-                src={curator?.image || images.profileImg2}
-                alt="curationCardProfileImage"
+                src={curator?.image || images.defaultProfile}
+                alt='큐레이션 카드의 프로필 이미지'
               />
             </ImageDiv>
             <NicknameDiv onClick={handleUserPage}>{curator?.nickname}</NicknameDiv>
@@ -69,52 +69,52 @@ const CurationCard = ({
 };
 
 const CardContainer = styled.div<{ type?: CurationType }>`
-  width: 300px;
+  width: ${(props) => (props.type === CurationType.MYPAGE ? `calc(50% - 1rem)` : `300px`)};
   height: 200px;
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  box-shadow: ${colors.mainGray300} 3px 3px 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1.3rem;
+  margin-bottom: 1.8rem;
+  text-align: center;
+  font-size: 0.9rem;
+  border-radius: 0.625rem;
+  background-color: ${colors.mainBlue100};
+  cursor: pointer;
+  justify-content: space-between;
 
   &:hover {
-    background-color: ${({ theme }) => theme.colors.mainBlue100};
-    color: white;
+    background-color: ${colors.mainKey};
+    color: ${colors.mainWhite};
     > div:nth-child(3) {
-      color: white;
+      color: ${colors.mainWhite};
     }
   }
-  ${tw`
-    flex
-    flex-col
-    items-center
-    px-[1rem]
-    py-[1rem]
-    mb-[1.8rem]
-    text-[0.9rem]
-    rounded-[0.625rem]
-    bg-[#d9e1e8]
-    cursor-pointer
-    justify-between
-  `}
 `;
 
 const Item = styled.div`
   &:first-child {
     font-size: 1.5rem;
+    margin-bottom: -3px;
   }
   &:nth-child(2) {
-    font-size: 0.9;
-    font-weight: 600;
+    font-size: 1rem;
+    font-family: ${fonts.subBold};
   }
   &:nth-child(3) {
     width: 100%;
     overflow: hidden;
+    padding: 0px 5px;
     white-space: normal;
     text-overflow: ellipsis;
     text-align: center;
     font-size: 0.8rem;
-    line-height: 1.2rem;
+    line-height: 1.3rem;
     display: -webkit-box;
     -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
-    color: #595656;
+    color: ${colors.mainGray400};
   }
   &:nth-child(4) {
     display: flex;
@@ -126,20 +126,35 @@ const Item = styled.div`
     justify-content: space-between;
   }
 `;
-const ItemLeft = tw.div`
 
+const ItemLeft = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
 `;
+
 const ItemRight = styled.div`
   display: flex;
   align-items: center;
   width: 30%;
 `;
-const LikeDiv = tw.div`
-  flex
-  items-center
-  gap-[0.3rem]
-  [> svg]:fill-[#df5858]
+
+const LikeDiv = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+
+  & > svg {
+    fill: ${colors.mainRed100};
+  }
 `;
+
+const LikeComment = styled.div`
+  @media (max-width: 500px) {
+    display: none;
+  }
+`;
+
 const NicknameDiv = styled.span`
   text-align: end;
   overflow: hidden;
@@ -150,19 +165,22 @@ const NicknameDiv = styled.span`
   -webkit-line-clamp: 1;
   -webkit-box-orient: vertical;
 `;
-const ImageDiv = tw.div`
-  rounded-full
-  w-6
-  h-6
-  mr-2
-  overflow-hidden
-  flex
-  justify-center
+
+const ImageDiv = styled.div`
+  border-radius: 9999px;
+  width: 1.5rem;
+  height: 1.5rem;
+  margin-right: 0.5rem;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
 `;
+
 const ProfileImg = styled.img`
   height: inherit;
   object-fit: cover;
   width: 100%;
   height: 100%;
 `;
+
 export default CurationCard;
